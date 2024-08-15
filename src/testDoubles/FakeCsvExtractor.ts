d142d73987b3573d4328dec6b5197e10563dd163
eric-yates <hello@ericthecurious.com>
2024-08-14 18:35:59 -0600

    patch: update fake

--- src/testDoubles/FakeCsvExtractor.ts
+++ src/testDoubles/FakeCsvExtractor.ts
@@ -1,8 +1,20 @@
-import { CsvExtractor, ExtractionRule, ExtractedResult } from '../types'
+import {
+    CsvExtractor,
+    ExtractionRule,
+    ExtractedResult,
+    CsvData,
+} from '../types'
 
 export default class FakeCsvExtractor implements CsvExtractor {
+    public csvPath: string
+    public csvData: CsvData
     public wasExtractCalled = false
     public extractCalledWith: ExtractionRule[][] = []
+
+    public constructor(csvPath: string, csvData: CsvData) {
+        this.csvPath = csvPath
+        this.csvData = csvData
+    }
 
     public extract(rules: ExtractionRule[]) {
         this.wasExtractCalled = true
