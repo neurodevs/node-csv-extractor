80c751f4166a2adf671ba21ce0c44a89dac07833
eric-yates <hello@ericthecurious.com>
2024-08-15 11:13:47 -0600

    patch: update fake

--- src/testDoubles/FakeCsvExtractor.ts
+++ src/testDoubles/FakeCsvExtractor.ts
@@ -6,12 +6,12 @@ import {
 } from '../types'
 
 export default class FakeCsvExtractor implements CsvExtractor {
-    public static fakedRecord: ExtractedRecord = {}
+    public static fakedRecord?: ExtractedRecord
 
     public csvPath: string
     public csvData: CsvData
     public passedRules: ExtractionRule[][] = []
-    private fakedRecord?: ExtractedRecord
+    private fakedRecord: ExtractedRecord = {}
 
     public constructor(csvPath: string, csvData: CsvData) {
         this.csvPath = csvPath
@@ -20,10 +20,11 @@ export default class FakeCsvExtractor implements CsvExtractor {
 
     public extract(rules: ExtractionRule[]) {
         this.passedRules.push(rules)
-        return this.fakedRecord ?? FakeCsvExtractor.fakedRecord
+        return FakeCsvExtractor.fakedRecord ?? this.fakedRecord
     }
 
     public setFakeRecord(record: ExtractedRecord) {
+        delete FakeCsvExtractor.fakedRecord
         this.fakedRecord = record
     }
 }
